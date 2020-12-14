d331a05b0952d3c96ffeefe770ac14c50992759e
Josh Sirota <josh@sirota.org>
2020-12-14 13:22:05 -0800

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,9 +1,13 @@
 <?php
+/*
+ * Simple PHP script to call the Particle API to fetch the state of the 'gateClosed' variable
+ */
 $result = file_get_contents("https://api.particle.io/v1/devices/YOUR_DEVICE_ID/gateClosed?access_token=YOUR_BIG_LONG_ACCESS_TOKEN");
 $json = json_decode($result);
 
 $gateIsClosed = $json->result;
 
+// Based on the results, set some variables that will affect how the web page looks
 if ($gateIsClosed) { 
     $bgColor = "green";
     $statusString = "CLOSED";
@@ -19,13 +23,14 @@ if ($gateIsClosed) {
     <meta http-equiv="refresh" content="5">
   </head>
   <body style="background-color: <?php echo $bgColor ?>">
-  <p style="display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  min-height: 100vh;
-  font-size: 13vh;">The gate is <b><?php echo $statusString ?>.</b></p>
+    <p style="display: flex;
+       flex-direction: column;
+       justify-content: center;
+       align-items: center;
+       text-align: center;
+       min-height: 100vh;
+       font-size: 13vh;">
+      The gate is <b><?php echo $statusString ?>.</b>
+    </p>
   </body>
 </html>
-
